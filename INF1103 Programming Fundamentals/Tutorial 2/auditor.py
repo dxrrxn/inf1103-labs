7d172c9e510aadc1167fc23d4d949eee565299c4
Renjie Ng <darrenng2003@gmail.com>
2026-09-10 13:23:55 +0800

    Loop Quantity Until Quit

--- INF1103 Programming Fundamentals/Tutorial 2/auditor.py
+++ INF1103 Programming Fundamentals/Tutorial 2/auditor.py
@@ -2,3 +2,11 @@ def main():
     inventory = 0
     total_units_processed = 0
     failed_entries = 0
+
+
+while True:
+        user_input = input("Enter stock quantity (or type 'quit' to stop): ").strip()
+ 
+        # Exit condition
+        if user_input.lower() == "quit":
+            break
