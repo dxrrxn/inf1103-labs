7c7e5d5ccad84235aff7cf5acc4d986dd32f7f62
Renjie Ng <darrenng2003@gmail.com>
2026-09-10 13:40:55 +0800

    Reject Non Numeric Inputs

--- INF1103 Programming Fundamentals/Tutorial 2/auditor.py
+++ INF1103 Programming Fundamentals/Tutorial 2/auditor.py
@@ -10,3 +10,10 @@ while True:
         # Exit condition
         if user_input.lower() == "quit":
             break
+
+        if not user_input.isdigit():
+            # Step 4: Reject non-numeric input (e.g. "ten")
+            print(f"Error: '{user_input}' is not a valid number. Please try again.")
+            failed_entries += 1
+            continue
+        
